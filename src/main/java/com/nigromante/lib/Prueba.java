88b6a1d715b260f21ba636baf557c02a6b5476b3
Nigromante <nigromante@github.com>
2026-02-23 01:04:27 -0300

    refactor

--- src/main/java/com/nigromante/lib/Prueba.java
+++ src/main/java/com/nigromante/lib/Prueba.java
@@ -1,15 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.nigromante.lib;
 
-/**
- *
- * @author julian
- */
+
 public class Prueba {
+
     public int tt() {
         return 1234;
     }
+
 }
+
